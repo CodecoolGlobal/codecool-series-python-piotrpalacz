2d25f463b6da7b8f3bb6a95e36dfefab320edb64
piotrpalacz <peterpalacz12@gmail.com>
2022-06-25 22:04:53 +0200

    Kamil queries for sortable columns

--- data/queries.py
+++ data/queries.py
@@ -1,4 +1,5 @@
 from data import data_manager
+from psycopg2 import sql
 
 
 def get_shows():
@@ -22,3 +23,27 @@ def get_shows_page_count(page=15):
     FROM shows
     """, {'page': page})
 
+
+def kamil(order, limit, offset, order_by):
+    sql.SQL("""
+                SELECT
+                    shows.id,
+                    shows.title,
+                    shows.year,
+                    shows.runtime,
+                    to_char(shows.rating::float, '999.9') AS rating_string,
+                    string_agg(genres.name, ', ' ORDER BY genres.name) AS genres_list,
+                    shows.trailer,
+                    shows.homepage
+                FROM shows
+                    JOIN show_genres ON shows.id = show_genres.show_id
+                    JOIN genres ON show_genres.genre_id = genres.id
+                GROUP BY shows.id
+                ORDER BY
+                    CASE WHEN %(order)s = 'ASC' THEN {order_by} END ASC,
+                    CASE WHEN %(order)s = 'DESC' THEN {order_by} END DESC
+                LIMIT %(limit)s
+                OFFSET %(offset)s;
+            """
+            ).format(order_by=sql.Identifier(order_by)),
+    {"order": order, "limit": limit, "offset": offset}
